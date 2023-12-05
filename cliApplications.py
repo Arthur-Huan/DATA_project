850b6c971e2d5cf8ba1bf5a54f63717e15ba5cdb
kirthik.ramachandran <kirthik.ramachandran@ucalgary.ca>
2023-12-04 17:38:50 -0700

    menu final

--- cliApplications.py
+++ cliApplications.py
@@ -1,15 +1,10 @@
 import sqlite3
 
 import typer
-<<<<<<< HEAD
 from typing_extensions import Annotated
 from typing import Optional
 from data_format_checks import *
-=======
 
-from data_format_checks import *
-
->>>>>>> 69f07526e234a0cf19c75e9a176048300b349c5d
 app = typer.Typer()
 
 
@@ -343,10 +338,6 @@ if __name__ == "__main__":
         if username:
             unfollow_user(cursor, username)
 
-
-    app()
-<<<<<<< HEAD
-
 def main_menu(cursor):
     typer.echo("=== Twitter-like CLI Menu ===")
     typer.echo("1. Register")
@@ -375,6 +366,16 @@ if __name__ == "__main__":
     cursor = init_cursor(db_name)
 
     while True:
-        main_menu(cursor)  
-=======
->>>>>>> 69f07526e234a0cf19c75e9a176048300b349c5d
+        main_menu(cursor)
+
+    # Add the following lines to handle your existing CLI commands after the main menu loop:
+    app.add_typer(post_tweet, name="post", help="Post a new tweet")
+    app.add_typer(view_timeline, name="timeline", help="View your timeline")
+    app.add_typer(like_tweet, name="like", help="Like a tweet")
+    app.add_typer(view_likes, name="view-likes", help="View likes on a tweet")
+    app.add_typer(add_comment, name="comment", help="Add a comment to a tweet")
+    app.add_typer(follow_user, name="follow", help="Follow a user")
+    app.add_typer(unfollow_user, name="unfollow", help="Unfollow a user")
+
+
+    app()
