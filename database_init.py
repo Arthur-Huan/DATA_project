d4214aaf6caa694033ca6c8fe6e97f5b47d8d313
Arthur-Huan <c.arthurh@outlook.com>
2023-12-11 23:16:18 -0700

    fix db setup file outside main module

--- database_init.py
+++ database_init.py
@@ -1,4 +1,3 @@
 from chirrup import database_setup, __database_path__
+database_setup.initialize_database(__database_path__)
 
-if __name__ == "__main__()":
-    database_setup.initialize_database(__database_path__)
